978c4d75f3b1720abd316c89de00e689e8ec4383
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-23 16:37:30 +0530

    home page

--- frontend/src/landing page/home/Awards.jsx
+++ frontend/src/landing page/home/Awards.jsx
@@ -2,6 +2,50 @@ import React from 'react'
 
 export const Awards = () => {
   return (
-    <div>Awards</div>
-  )
+    <div className="container mx-auto p-5">
+      <div className="w-full flex">
+        <div className="w-1/2 ">
+          <img src="largestBroker.svg" />
+        </div>
+        <div className="w-1/2 pr-7">
+          <h1 className="text-4xl font-bold py-3">Invest in everything</h1>
+          <p className='pb-7'>
+            That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores
+            of equity investments and contribute to 15% of daily retail exchange
+            volumes in India.
+          </p>
+          <div className="w-full flex py-5">
+            <div className="w-1/2">
+              <ul className='list-disc pl-5'>
+                <li>
+                  1
+                </li>
+                <li>
+                  2
+                </li>
+                <li>
+                  3
+                </li>
+              </ul>
+            </div>
+            <div className="w-1/2">
+              <ul className='list-disc pl-5'>
+                <li>
+                  <p>4</p>
+                </li>
+                <li>
+                  <p>5</p>
+                </li>
+                <li>
+                  <p>6</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <img src='pressLogos.png'/>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/landing page/home/Hero.jsx
+++ frontend/src/landing page/home/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="container mx-auto">
+      <div className="w-full text-center py-20">
+        <img className="mb-7" src='homeHero.png' />
+        <h1 className="text-4xl font-bold mt-6 mb-2">Invest in everything</h1>
+        <p className="text-lg mt-4 text-center text-gray-600">Invest in stocks, ETFs, options, and crypto—all commission-free, right from your phone or desktop.</p>
+        <button className='bg-blue-500 text-white px-6 py-3 mt-4 rounded-md font-medium hover:bg-blue-600 transition-colors'>Signin Now</button>
+      </div>
+    </div>
   )
 }
 

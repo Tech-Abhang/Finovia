ff5515cbcb6680efa377094848f7dc513064ab7d
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-24 12:42:53 +0530

    education commponent

--- frontend/src/landing page/home/Education.jsx
+++ frontend/src/landing page/home/Education.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const Education = () => {
   return (
-    <div>Education</div>
-  )
+    <div className="container mx-auto p-5 py-15">
+      <div className="w-full flex">
+
+        <div className="w-1/2">
+          <img src='education.svg' className='w-4/5'></img>
+        </div>
+
+        <div className="w-1/2">
+          <h2 className="text-2xl font-semibold pb-10">Unbeatable Pricing</h2>
+          <p className="text-gray-500 py-2">
+            We pioneer the concept of discount broking in India.
+          </p>
+          <a href="" className="text-blue-600 hover:underline">
+            Explore our products
+          </a>
+
+          <p className="text-gray-500 py-2 pt-12">
+            Trading Q&A and most active trading commutinity in India for all  market related queries.
+          </p>
+          <a href="" className="text-blue-600 hover:underline">
+            Trading Q&A
+          </a>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education
--- frontend/src/landing page/home/Pricing.jsx
+++ frontend/src/landing page/home/Pricing.jsx
@@ -25,7 +25,6 @@ const Pricing = () => {
           </div>
         </div>
       </div>
-
     </div>
   )
 }

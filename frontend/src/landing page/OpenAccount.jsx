017cdc7da9d275387d3514d1331eef310c1f0c3a
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-25 13:30:12 +0530

    about page

--- frontend/src/landing page/OpenAccount.jsx
+++ frontend/src/landing page/OpenAccount.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const OpenAccount = () => {
   return (
-    <div>OpenAccount</div>
-  )
+    <div className="container mx-auto p-5 py-5 pb-[100px]">
+      <div className="flex flex-col items-center justify-center ">
+        <h1 className="text-2xl font-semibold py-3">Open a zerodha account</h1>
+        <p className='pb-3'>
+          Modern platforms and app , no investment and flat 20 on Intraday and
+          F&O
+        </p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Open Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default OpenAccount
--- frontend/src/landing page/about/Hero.jsx
+++ frontend/src/landing page/about/Hero.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='container mx-auto p-5 py-20'>
+      <h1 className='text-2xl font-semibold text-center pb-23'>We pioneer the discount broking model in India , now breaking ground with our technology</h1>
+      <div className='border-t flex pt-10'>
+        <div className='w-1/2 pr-10'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero commodi, magnam nostrum quibusdam officiis sequi unde esse, velit, voluptas ducimus non iste aliquam architecto saepe in assumenda laudantium autem. Placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. In facere quas ab blanditiis obcaecati dolores architecto ipsa ut ullam, iusto minus aut veniam sequi quaerat deleniti optio eaque accusamus voluptates.
+          Voluptatem iusto temporibus vel dolore sunt incidunt quia ipsa corrupti aperiam quos. Voluptates nulla atque velit perferendis id? Dolore suscipit, qui necessitatibus adipisci quidem consequuntur doloribus numquam accusamus voluptatem blanditiis.
+          Nobis rerum laborum sit iusto sequi dignissimos, vero harum atque unde accusamus nostrum dolore? Eum qui dicta reiciendis quisquam tempore laudantium deleniti nobis magni numquam dolore explicabo dolores, ab quas!</p>
+        </div>
+        <div className='w-1/2 pl-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusamus similique distinctio sapiente quod quas, eos delectus ullam nobis odio, aliquam illo esse, a nisi eveniet obcaecati natus facilis quo!non iste aliquam architecto saepe in assumenda laudantium autem. Placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. In facere quas ab blanditiis obcaecati dolores architecto ipsa ut ullam, iusto minus aut veniam sequi quaerat deleniti optio eaque accusamus voluptates.
+          Voluptatem iusto temporibus vel dolore sunt incidunt quia ipsa corrupti aperiam quos. Voluptates nulla atque velit perferendis id? Dolore suscipit, qui necessitatibus adipisci quidem consequuntur doloribus numquam accusamus voluptatem blanditiis.
+          Nobis rerum laborum sit iusto sequi dignissimos, vero harum atque unde accusamus nostrum dolore? Eum qui dicta reiciendis quisquam tempore laudantium deleniti nobis magni numquam dolore explicabo dolores, ab quas!</div>
+      </div>
+    </div>
   )
 }
 

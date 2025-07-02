a405a55ac0616c02635ec0c7618c54d584f99dc7
Abhang Pawar <abhangpawar03@gmail.com>
2025-07-02 23:26:11 +0530

    small change

--- LLM-Backend/models/Insider/insider.py
+++ LLM-Backend/models/Insider/insider.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b043c8587eb98ade136731d4bafc419e78a2404c0252a8bc88dcdfcd6c760c4
-size 4099
+oid sha256:42f0ef7c33e4b4bb7e83f3590c33f0433fdcdfa82c6aa2c85a6e97b3c620d66c
+size 4132
